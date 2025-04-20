aeece205a1d69f9fbf90f585a2fdbc4fa8526634
Ayoub <jounaidayoub1@gmail.com>
2025-04-21 00:30:25 +0100

    amazing stuff

--- main.js
+++ main.js
@@ -1,23 +1,44 @@
-cercles=document.querySelectorAll('.cercle');
+const cercles = document.querySelectorAll(".cercle");
+const mouse = { x: 0, y: 0 };
 
-window.addEventListener('mousemove', (event) => {
-    let x = event.clientX;
-    let y = event.clientY;
+cercles.forEach(cercle => {
+  cercle.x = 0;
+  cercle.y = 0;
+});
 
-    // let x
+window.addEventListener("mousedown", () => {
+  cercles.forEach(c => c.classList.add("scale"));
+});
+window.addEventListener("mouseup", () => {
+  cercles.forEach(c => c.classList.remove("scale"));
+});
 
+window.addEventListener("mousemove", (event) => {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+});
 
-    cercles.forEach((cercle) => {
-        cercle.style.left = `${x - 6}px`;
-        cercle.style.top = `${y - 6}px`;
+// animate
+function animate() {
+  let x = mouse.x;
+  let y = mouse.y;
 
-        
-        // x = x + ;
-        // y = y + 10;
-    })
+  cercles.forEach((cercle,index) => {
 
+    cercle.x += (x - cercle.x) * 0.5;
 
+    cercle.y += (y - cercle.y) * 0.5;
 
+    cercle.style.left = `${cercle.x - 6}px`;
+    cercle.style.top = `${cercle.y - 6}px`;
 
-    console.log(`Mouse X: ${x}, Mouse Y: ${y}`);
-})
+    cercle.style.scale = 1 - index / cercles.length**5;
+
+    x = cercle.x;
+    y = cercle.y;
+  });
+
+  requestAnimationFrame(animate);
+}
+
+animate();
